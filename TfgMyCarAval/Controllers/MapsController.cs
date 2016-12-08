246c2284e644104148c986dc9019187a00c98bd9
Hany Altadmuri <hany@synergiart.com>
2016-12-08 17:53:07 +0100

    Maps and home design

---
+++ TfgMyCarAval/Controllers/MapsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCarAval.Controllers
+{
+    public class MapsController : Controller
+    {
+        // GET: Maps
+        public ActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+        public ActionResult ViewMaps()
+        {
+            return View();
+        }
+    }
+}
